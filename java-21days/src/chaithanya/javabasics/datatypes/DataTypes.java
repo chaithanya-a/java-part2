b1960d1954dcf17d6c9e2f0b5664dfcaf988b914
spoorthy <spoorthy@spoorthy-PC>
2020-03-27 13:14:26 +0530

    removed helloworld and added new data types package

---
+++ java-21days/src/chaithanya/javabasics/datatypes/DataTypes.java
@@ -0,0 +1,16 @@
+package chaithanya.javabasics.datatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) 
+	{
+		int ac_no=5;
+		float balance=(float) 2.560;
+		String name="CHAITHANYA";
+		String city="hassan";
+		System.out.println("hello my name is "+name+ " from "+city+" with the account number : "+ac_no+" and my balance is : "+balance);
+		
+
+	}
+
+}
